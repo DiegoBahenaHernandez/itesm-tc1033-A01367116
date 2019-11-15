d58519e6d584efd495db0f7fa30883eb0f22acb9
Diego Bahena <aguiladiego33@outlook.es>
2019-11-15 14:39:45 -0600

    Se agregaron los porcentajes

--- ejemplo.py
+++ ejemplo.py
@@ -1,9 +1,31 @@
 #-*-coding-8-*-
+meses={}
+pais={}
+
 archivo=open("datos_vuelos.csv","r+")
 all_lines=archivo.readlines()
+all_lines.pop(0)
+
 for x in all_lines:
     datos=x.split(",")
-    matri=datos[0][0:2]
+    mat=datos[0][0:2]
     mes=datos[2][3:5]
-    print(matri,mes)
-    
+    if mes not in meses:
+        meses[mes]=1
+    else:
+        meses[mes]+=1
+        
+    if mes not in pais:
+        pais[mes]={}
+        
+    if mat not in pais [mes]:
+        pais[mes][mat]=1
+    else:
+        pais[mes][mat]+=1
+print(pais)
+print(meses)
+
+for mes in pais:
+    for mat in pais[mes]:
+        porcentaje=pais[mes][mat]/meses[mes] 
+        print(mes, mat, porcentaje)
