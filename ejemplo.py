978432146fea05be6754e82c54ccdc25f7e1255a
Diego Bahena <aguiladiego33@outlook.es>
2019-11-15 13:26:23 -0600

    Se obtiene matrícula y mes

--- ejemplo.py
+++ ejemplo.py
@@ -1,1 +1,9 @@
-sdfghjkl
+#-*-coding-8-*-
+archivo=open("datos_vuelos.csv","r+")
+all_lines=archivo.readlines()
+for x in all_lines:
+    datos=x.split(",")
+    matri=datos[0][0:2]
+    mes=datos[2][3:5]
+    print(matri,mes)
+    
